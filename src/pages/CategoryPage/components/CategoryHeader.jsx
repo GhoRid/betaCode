e5d92c99c25bfd1c1a2c22eebd185eae594cf68f
GhoRid <pgh5489@gmail.com>
2024-07-23 18:48:55 +0900

    ddd

--- src/pages/CategoryPage/components/CategoryHeader.jsx
+++ src/pages/CategoryPage/components/CategoryHeader.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import Text from "../../../components/Text";
@@ -13,7 +13,7 @@ const Container = styled.div`
   grid-template-columns: repeat(3, 1fr);
   box-shadow: 0 5px 15px -4px rgba(0, 0, 0, 0.4),
     0 -5px 15px -4px rgba(0, 0, 0, 0.4);
-  position: relative; /* Needed for positioning the moving border */
+  position: relative; /* MovingBorder 위치를 설정하기 위해 필요 */
 `;
 
 const Box = styled.button`
@@ -27,17 +27,23 @@ const Box = styled.button`
   &:last-child {
     border-right: none;
   }
+
+  ${(props) =>
+    props.active &&
+    `
+    background-color: #f0f0f0;
+  `}
 `;
 
 const MovingBorder = styled.div`
   position: absolute;
   bottom: 0;
-  left: ${(props) => props.left}px; /* Move the border based on the left prop */
-  width: ${(props) => props.width}px; /* Set the width of the border */
+  left: ${(props) => props.left}px; /* 경계선의 왼쪽 위치 */
+  width: ${(props) => props.width}px; /* 경계선의 너비 */
   height: 5px;
   border-radius: 5px;
   background-color: #4887e4;
-  transition: left 0.3s ease, width 0.3s ease;
+  transition: left 0.3s ease, width 0.3s ease; /* 부드러운 이동을 위한 transition */
 `;
 
 const CategoryHeader = ({ category }) => {
@@ -45,10 +51,27 @@ const CategoryHeader = ({ category }) => {
   const [activeCategory, setActiveCategory] = useState(category);
   const [borderPosition, setBorderPosition] = useState({ left: 0, width: 0 });
 
+  // 컴포넌트가 처음 렌더링될 때 초기 경계선 위치를 설정합니다.
+  useEffect(() => {
+    // 카테고리 박스의 초기 위치와 너비를 설정합니다.
+    const setInitialBorderPosition = () => {
+      const initialIndex = ["밥집", "술집", "카페"].indexOf(category);
+      const box = document.getElementById(`box-${initialIndex}`);
+      if (box) {
+        const { offsetLeft, offsetWidth } = box;
+        setBorderPosition({ left: offsetLeft, width: offsetWidth });
+      }
+    };
+
+    setInitialBorderPosition();
+  }, [category]);
+
   const handleCategoryClick = (category, index) => {
     const box = document.getElementById(`box-${index}`);
-    const { offsetLeft, offsetWidth } = box;
-    setBorderPosition({ left: offsetLeft, width: offsetWidth });
+    if (box) {
+      const { offsetLeft, offsetWidth } = box;
+      setBorderPosition({ left: offsetLeft, width: offsetWidth });
+    }
 
     setActiveCategory(category);
     navigate(`/category/${category}`, { state: { category } });
