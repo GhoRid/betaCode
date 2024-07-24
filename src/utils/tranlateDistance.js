57852df831467299d77de30ac9a9be25ed86cfc6
GhoRid <pgh5489@gmail.com>
2024-07-24 16:46:57 +0900

    sort out distance error2

--- src/utils/tranlateDistance.js
+++ src/utils/tranlateDistance.js
@@ -1,6 +1,6 @@
 export const translateDistance = (distance) => {
   if (isNaN(distance)) {
-    return "거리 표시 불가";
+    return "";
   } else if (distance < 1000) {
     return `${distance.toFixed(0)}m`;
   }
